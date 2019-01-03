6f5c2de72a6e1a3091b929c1d665e2830700815f
Marko Arsic <marsicdev@gmail.com>
2019-01-03 15:31:59 +0100

    Finish docs for GET routes

--- src/lib/middleware/apiKeyAuth.js
+++ src/lib/middleware/apiKeyAuth.js
@@ -1,23 +1,30 @@
+import { Buffer } from 'safe-buffer'
+
 import { db } from '../services/database'
 
 export default ({ headers }, res, next) => {
-    const apiKey = headers['x-api-key']
+    // Key is sent in Base64 btoa() format
+    // as an extra layer of protection
+    const encodedApiKey = headers['x-api-key']
 
-    if (!apiKey) {
+    if (!encodedApiKey) {
         const status = 401
         const message = 'Invalid auth header'
         res.status(status).json({ status, message })
         return
     }
 
+    // Decode API key before use
+    const decodedKey = Buffer.from(encodedApiKey, 'base64').toString()
+
     // Find key in DB
     const keyObj = db
         .get('keys')
-        .find({ key: apiKey })
+        .find({ key: decodedKey })
         .value()
 
     // Validate key
-    if (keyObj && keyObj.key === apiKey) {
+    if (keyObj && keyObj.key === decodedKey) {
         next()
         return
     }
